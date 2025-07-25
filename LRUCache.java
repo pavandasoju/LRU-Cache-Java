5d22a5a90a42d61cbba6b9137f4aa6b0c7b94c46
pavandasoju <akhildasoju@gmail.com>
2025-07-25 18:36:33 +0530

    Update LRUCache.java

--- LRUCache.java
+++ LRUCache.java
@@ -1,75 +1,109 @@
+import java.util.*;
 
+class LRUCache {
+    int capacity;
+    int size;
+    CDLL list;
+    Map<Integer, CDLLNode> map;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.list = new CDLL();
+        this.map = new HashMap<>();
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+        CDLLNode node = map.get(key);
+        list.moveAtFront(node);
+        return node.val;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            CDLLNode node = map.get(key);
+            node.val = value;
+            list.moveAtFront(node);
+        } else if (size < capacity) {
+            CDLLNode node = list.insAtBegin(key, value);
+            map.put(key, node);
+            size++;
+        } else {
+            int deletedKey = list.delNode();
+            map.remove(deletedKey);
+            CDLLNode node = list.insAtBegin(key, value);
+            map.put(key, node);
+        }
+    }
+}
 
 class CDLLNode {
-    int key,val;
-    CDLLNode prev,next;
-    public CDLLNode(int k , int v){
-        this.key=k;
-        this.val=v;
-    }}
-class CDLL{
+    int key, val;
+    CDLLNode prev, next;
+
+    CDLLNode(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+class CDLL {
     CDLLNode head;
-    public CDLL(){
-        head=null;
+
+    public CDLL() {
+        head = null;
     }
-    void insAtBegin(int k,int v){
-        CDLLNode nn = new CDLLNode(k,v);
-        nn.next=nn;nn.prev=nn;
-        if(head==null){
-            head= nn;
-            return;
+
+    CDLLNode insAtBegin(int key, int val) {
+        CDLLNode newNode = new CDLLNode(key, val);
+        newNode.next = newNode.prev = newNode;
+        if (head == null) {
+            head = newNode;
+            return head;
         }
         CDLLNode last = head.prev;
-        nn.next = head; head.prev=nn;
-        last.next=nn; nn.prev=last;
-        head= nn;
+        newNode.next = head;
+        head.prev = newNode;
+        last.next = newNode;
+        newNode.prev = last;
+        head = newNode;
+        return head;
     }
-    void printLL(){
-       if(head== null) return;
-       System.out.print(head.key+" ");
-       CDLLNode temp= head.next;
-        while(temp!=head){
-            System.out.print(temp.key+" ");
-            temp=temp.next;
-        }
-        System.out.println();
-    }
-    void delNode(){
-        if(head==null) return;
+
+    int delNode() {
+        if (head == null) return -1;
         CDLLNode last = head.prev;
-        if(last==head){
-            head=null;
-            return;
+        if (last == head) {
+            int key = head.key;
+            head = null;
+            return key;
         }
-        last.prev.next= head;
+        int key = last.key;
+        last.prev.next = head;
         head.prev = last.prev;
-
+        return key;
     }
-    void moveATFront(CDLLNode nodetoMove){
-        if (nodetoMove==head) return;
-        
-        nodetoMove.prev.next=nodetoMove.next;
-        nodetoMove.next.prev= nodetoMove.prev;
+
+    void moveAtFront(CDLLNode node) {
+        if (node == head) return;
+        // Detach node
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+
+        // Insert at front
         CDLLNode last = head.prev;
-        nodetoMove.next=head;
-        head.prev= nodetoMove;
-        last.next=nodetoMove;
-        nodetoMove.prev=last;
-        head= nodetoMove;
+        node.next = head;
+        head.prev = node;
+        last.next = node;
+        node.prev = last;
+        head = node;
     }
-}
 
-
-    class LRUCache{
-    public static void main(String[] args) {
-        CDLL list1= new CDLL();
-        list1.insAtBegin(0,0); list1.insAtBegin(1,111); list1.insAtBegin(3,303);
-        list1.printLL();
-        list1.delNode();
-        list1.printLL();
-        list1.moveATFront(list1.head.next);
-        list1.printLL();
-        list1.moveATFront(list1.head.next);
-        
-    }
-}
+    // Optional: Utility to print list keys (for debugging)
+    void printLL() {
+        if (head == null) return;
+        System.out.print("Cache Order: " + head.key + " ");
+        CDLLNode temp = head.next;
+        while (temp != head) {
